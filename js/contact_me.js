1ffac295554ec4c160487222e47eb2853ce01107
Denis Trailin <hiyou102@gmail.com>
2017-12-13 12:16:20 -0800

    Fix syntax error

--- js/contact_me.js
+++ js/contact_me.js
@@ -23,6 +23,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
              
+        },
         filter: function() {
             return $(this).is(":visible");
         },
